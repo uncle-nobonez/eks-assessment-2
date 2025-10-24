05deab8774877981266bce1a7e7d89208428564a
JJayBlinks <jayblinks1960@gmail.com>
2025-10-24 10:46:17 -0700

    update8

--- terraform/eks/minimal/alb.tf
+++ terraform/eks/minimal/alb.tf
@@ -1,4 +1,4 @@
 // Terraform-managed ALB removed per user request.
 // Intentionally left blank to revert the ALB configuration.
 
-// Previous content has been deleted.
+// Previous content has been deleted
